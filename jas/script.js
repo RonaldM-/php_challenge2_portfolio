34e85b24911f2ffb2c6287da6a18f4a15e3f40e4
RonaldM- <ronald.marcel.mr@gmail.com>
2016-09-06 18:33:48 +0200

    JS - comment script.js

--- jas/script.js
+++ jas/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+/**/
     $("#click_left").click(function(){
         $("#left").removeClass("closed").addClass('col-md-9 opened');
         $("#center").removeClass("col-md-12").addClass("col-md-3");
         $(".info").removeClass("col-md-4 col-md-offset-4").addClass("col-md-12");
     });
+/*Hover Effect*/
     $(".project").mouseover(function(){
         $(this).find(".pbox_title").clearQueue().delay(100).animate({bottom: '120px'});
     });
