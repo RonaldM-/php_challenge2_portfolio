aabe30a0cf1a69adcef41342b407d8b8c4563e2e
RonaldM- <ronald.marcel.mr@gmail.com>
2016-09-15 00:18:07 +0200

    debugg + ajout  de la partie designer + debugg gallery

--- build/js/script.js
+++ build/js/script.js
@@ -1,16 +1,43 @@
 $(document).ready(function(){
-/**/
+/*ouverture et fermeture de mes parties design et developpement*/
     $("#coder").on("click", function(){
-        $(".home").switchClass("home","home_small",2000);
-        $(".closed").switchClass("closed","open", 2000 );
-        $(".home_small").switchClass("home_small","home", 2000 );
-        $(".open").switchClass("open", "closed", 2000 );
+        $(".home").css({float:'right'});
+        $(".home").switchClass("home","home_small",1500);
+        $(".closed_left").switchClass("closed_left","open", 1500 );
+        $(".home_small").switchClass("home_small","home", 1500 );
+        $(".open").switchClass("open", "closed_left", 1500 );
     });
 /**/
-    $("designer").click(function(){
-        $(".home").switchClass("home","home_small_left", 2000);
-        $(".home_small_left").switchClass("home_small_left", "home",2000);
+    $("#designer").click(function(){
+        $(".home").css({float:'left'});
+        $(".home").switchClass("home","home_small_left", 1500);
+        $(".closed_right").switchClass("closed_right","open", 1500 );
+        $(".home_small_left").switchClass("home_small_left", "home",1500);
+        $(".open").switchClass("open", "closed_right", 1500 );
     });
+
+
+
+/*Effet sur la gallerie fading et hover*/
+
+//    $(window).load(function(){
+//        $('.box').fadeIn(3000);
+//    });
+    $('.box').mouseover(function(){
+        $(this).find('.box_img').clearQueue().delay(50).animate({bottom: '17%'}, 1000);
+    });
+    $('.box').mouseout(function(){
+        $(this).find('.box_img').clearQueue().delay(50).animate({bottom: '0px'}, 1000);
+    });
+
+
+
+
+
+
+
+
+
 
 
 
